8a3af44d32277a8062a81e8a3c353838e6c1f92c
Simonbdbc <simonbdbc@gmail.com>
2018-03-09 02:49:29 +0100

    add plugin and update readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # wp-plugin-git-commit-widget
-Simple wordpress widget who display github commit from a repository
+Simple wordpress widget who display github last commits from a selected repository
+
+### How to use ? 
+
+* Copy `sbb-github-commits` directory  in the wordpress plugin's directory.
+
+* After activate, go in the widgets section.
+
+* Enter Github account name (username).
+
+* Enter the repository's name.
+
+* Enter how many commit you want to display.
+
+* And save the form.
+
+### Thanks !
+
+* Php library for Github API : https://github.com/milo/github-api
---
+++ sbb-github-commits/sbb-github-commits.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Plugin Name: github-commits
+ * repository: Affichage de commits dans un widget
+ * Version: 0.1
+ * Author: Simonbdbc
+ */
+
+class Github_Commits extends WP_Widget {
+ 
+        // Configuration globale du widget
+        function __construct() {
+ 
+                $widget_args = array(
+                        'classname' => 'widget_git_commits',
+                        'repository' => 'Affiche les derniers commits du repo selectionné'
+                );
+ 
+                parent::__construct(
+                        'git_commits',
+                        __('Github Commits'),
+                        $widget_args
+                );
+        }
+ 
+        // Affichage en front-end
+        function widget($args, $instance) {
+                
+                extract($args);
+                
+                $username = strip_tags($instance['username']);
+                $repository = strip_tags($instance['repository']);
+                $number = strip_tags($instance['number']);
+ 
+                echo $before_widget;
+
+                if ($number > 1) {
+                        echo $before_title . $number .' derniers commits du dépôt ' . $repository . $after_title;
+
+                } else {
+                        echo $before_title . 'Dernier commit du dépôt ' . $repository . $after_title;
+                }
+
+                if (!empty($username) && !empty($repository) && !empty($number)) {
+                        require_once 'vendor/autoload.php';
+                        $api = new Milo\Github\Api;
+                    
+                        $response = $api->get('/repos/'.($username).'/'.($repository).'/commits');
+                        $repo_commits = $api->decode($response);
+                    
+                        $nb = $number;
+                        $i = 0;
+                    
+                        echo '<ul>';
+                        foreach($repo_commits as $commits) {
+                                echo '<li>';
+                                echo ($commits->commit->message);
+                                echo '<br>';
+                                echo '<small>'.($commits->commit->committer->name).'</small>';
+                                echo '</li>';
+                                $i++;
+                                if ($i >= $nb){
+                                        break;
+                                }
+                        }
+                        echo '</ul>';
+                } else {
+                        echo 'Aucun commit à afficher';
+                }
+
+                echo $after_widget;
+        }
+ 
+        // Traitement des données avant sauvegarde
+        function update( $new_instance, $old_instance ) {
+                $new_instance['username'] = strip_tags($new_instance['username']);
+                $new_instance['repository'] = strip_tags($new_instance['repository']);
+                $new_instance['number'] = strip_tags($new_instance['number']);
+                return $new_instance;
+        }
+ 
+        // Affichage du formulaire de réglages du widget en back-end
+        function form($instance) {
+                $instance = wp_parse_args(
+                        $instance,
+                        array(
+                                'username' => '',
+                                'repository' => '',
+                                'number' => ''
+                        )
+                );
+ 
+                $username = strip_tags($instance['username']);
+                $repository = strip_tags($instance['repository']);
+                $number = strip_tags($instance['number']);
+ 
+                ?>
+ 
+                <p>
+                        <label for="<?= $this->get_field_id('username'); ?>">Pseudo Github :</label>
+                        <input class="widefat" id="<?= $this->get_field_id('username'); ?>" name="<?= $this->get_field_name('username'); ?>" type="text" value="<?= esc_attr($username); ?>" />
+                </p>
+ 
+                <p>
+                        <label for="<?= $this->get_field_id('repository'); ?>">Dépôt Github :</label>
+                        <input class="widefat" id="<?= $this->get_field_id('repository'); ?>" name="<?= $this->get_field_name('repository'); ?>" type="text" value="<?= esc_attr($repository); ?>" />
+                </p>
+ 
+                <p>
+                        <label for="<?= $this->get_field_id('number'); ?>">Nombre de commits affichés :</label>
+                        <input class="widefat" id="<?= $this->get_field_id('number'); ?>" name="<?= $this->get_field_name('number'); ?>" type="number" value="<?= esc_attr($number); ?>" />
+                </p>
+  
+        <?php }
+}
+ 
+function init_commits_widget() {
+        register_widget('Github_Commits');
+}
+
+add_action('widgets_init', 'init_commits_widget');
